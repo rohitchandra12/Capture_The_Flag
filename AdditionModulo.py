3a95b1b7f502e8d24f0a63e75f7a89233affc0b9
Rohit Chandra <89371656+rohitchandra12@users.noreply.github.com>
2026-03-01 13:03:38 +0530

    Complete Code

---
+++ AdditionModulo.py
@@ -0,0 +1,14 @@
+# Author: Rohit Chandra
+
+# Addition Modulo
+def additionModulo(p1, p2):
+    maxLen = max(len(p1), len(p2))
+    p1 = [0] * (maxLen - len(p1)) + p1
+    p2 = [0] * (maxLen - len(p2)) + p2
+    return list(map(lambda x, y : x ^ y, p1, p2))
+
+
+if __name__ == "__main__":
+    A = [1, 0, 1, 1]
+    B = [1, 1, 1, 0, 0]
+    print(additionModulo(A, B))
